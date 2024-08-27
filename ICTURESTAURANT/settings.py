1d8c9461d98432cf188241e0fd0269a1837bb733
FINAMOU01 <finamouabdoul.bellegrace@ictuniversity.edu.cm>
2024-08-27 03:13:43 -0700

    third commit

--- ICTURESTAURANT/settings.py
+++ ICTURESTAURANT/settings.py
@@ -128,6 +128,13 @@ STATICFILES_DIRS = [
     
     
 ]
+# settings.py
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.example.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your-email@example.com'
+EMAIL_HOST_PASSWORD = 'your-password'
 
 
 
