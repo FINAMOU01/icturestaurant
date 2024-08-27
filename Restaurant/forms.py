ece57c7d92b7ac326506f87bd6277b6098ffaba4
FINAMOU01 <finamouabdoul.bellegrace@ictuniversity.edu.cm>
2024-08-27 03:31:24 -0700

    tird commit

--- Restaurant/forms.py
+++ Restaurant/forms.py
@@ -3,4 +3,11 @@ class commandeForm(forms.Form):
     quantite = forms.IntegerField(min_value=1, label='Quantite')
     nom_client = forms.CharField(max_length=100, required=True)
     email_client = forms.EmailField(required=True)
-    mode_paiement = forms.CharField(label='MODES_PAIEMENT')
+    deposit_receipt= forms.ImageField(
+        label="Click here to download deposit receipt image :" ,
+        widget=forms.ClearableFileInput(attrs={
+        'class':'form-control'}),
+        required=False
+        
+        
+    )
--- Restaurant/models.py
+++ Restaurant/models.py
@@ -8,14 +8,14 @@ class Plat(models.Model):
     nom=models.CharField(max_length=100)
     description=models.TextField()
     prix=models.DecimalField(max_digits=6, decimal_places=0)
-    disponible=models.IntegerField(default=5)
+    disponible=models.BooleanField(default=True)
     image = models.ImageField(upload_to='plats/')
     
    
 
     def __str__(self):
         return self.nom
-  #lass menu  
+  #class menu  
     
 class Menu(models.Model):
     nom=models.CharField(max_length=100)
@@ -28,22 +28,19 @@ class Menu(models.Model):
 class commande(models.Model):
     STATUT_CHOICES = [
         ('en_attente', 'En attente'),
-        ('preparee', 'Préparée'),
         ('livree', 'Livrée'),
         ('annulee','Annulee')
-    ]
-    MODES_PAIEMENT = [
-        ('orange_money', 'Orange Money'),
-        ('mobile_money', 'Mobile Money'),
     ]
     plat = models.ForeignKey(Plat, on_delete=models.CASCADE)
     quantite = models.PositiveIntegerField()
     nom_client = models.CharField(max_length=100 ,default='client')
     email_client = models.EmailField(default='unknow@example.com')
     date_commande = models.DateTimeField(auto_now_add=True)
-    heure_disponibilite = models.TimeField(default=timezone.now().time())
+    heure_disponibilite = models.TimeField(default=timezone.now)
     statut = models.CharField(max_length=20, choices=STATUT_CHOICES,default='en_attente')
-    mode_paiement = models.CharField(max_length=20, choices=MODES_PAIEMENT, null=True, blank=True)
+    deposit_receipt = models.ImageField(upload_to='receipts/', null=True, blank=True)
 
     def __str__(self):
-        return f"order of {self.quantite} x {self.plat.nom} ,{self.quantite}*{self.plat.prix} by {self.nom_client}"
+        return f"order of {self.quantite} x {self.plat.nom} by {self.nom_client}"
+
+    
